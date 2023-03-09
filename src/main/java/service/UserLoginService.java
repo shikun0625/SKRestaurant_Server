6207da9d8929ed0f4d3454d3e82eb1aa72975163
施坤 <kshi@gw.nsw.co.jp>
2023-03-09 17:09:12 +0900

    aaa

--- src/main/java/service/UserLoginService.java
+++ src/main/java/service/UserLoginService.java
@@ -1,6 +1,9 @@
 package service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+
+import org.apache.tomcat.jakartaee.commons.io.IOUtils;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -13,25 +16,38 @@ import util.HttpUtil;
  */
 public final class UserLoginService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserLoginService() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserLoginService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		Error error = new HttpUtil().checkHttpRequestAuthorizedError(request, false);
-		if (error == null) {
-			
+		Error error;
+
+		BufferedReader bReader = request.getReader();
+		String body = new HttpUtil().getBodyString(request);
+		if (body == null) {
+			error = HttpUtil.HTTPBodyReadError;
 		}
+		error = new HttpUtil().checkHttpRequestAuthorizedError(request, body, false);
+		if (error == null) {
+
+		}
+		
+		
+		
+		new HttpUtil().setStatus(response, error);
+		
 	}
 
 }
--- src/main/java/util/HttpUtil.java
+++ src/main/java/util/HttpUtil.java
@@ -2,6 +2,8 @@ package util;
 
 import javax.persistence.Query;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
@@ -11,15 +13,17 @@ import javax.persistence.Persistence;
 
 import database.SkAuthorizedInfo;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public final class HttpUtil {
 
 	public final static Error AuthorizedError = new Error("用户验证失败");
 	public final static Error AuthorizedExpiredError = new Error("用户验证过期");
+	public final static Error HTTPBodyReadError = new Error("请求数据读取失败");
 
 	private static EntityManagerFactory eManagerFactory = Persistence.createEntityManagerFactory("SKRestaurant_Server");
 
-	public Error checkHttpRequestAuthorizedError(HttpServletRequest request, boolean checkUser) {
+	public Error checkHttpRequestAuthorizedError(HttpServletRequest request, String bodyStr, boolean checkUser) {
 
 		EntityManager entityManager = eManagerFactory.createEntityManager();
 		EntityTransaction transaction = entityManager.getTransaction();
@@ -31,9 +35,18 @@ public final class HttpUtil {
 		String osString = request.getHeader("os");
 		String osVersionString = request.getHeader("os_version");
 		String requestIdString = request.getHeader("request_id");
+		String userTokenString = request.getHeader("user_token");
+		
+		String headerString = timeString + deviceIdString + deviceNameString + osString + osVersionString + requestIdString;
+		if (userTokenString != null) {
+			headerString = headerString + userTokenString;
+		}
+		
+
+		String requeString = "skrestaurant_key" + bodyStr;
 
 		if (checkUser) {
-			String userTokenString = request.getHeader("user_token");
+			
 			Query query = entityManager.createNamedQuery("SkAuthorizedInfo.findAuthorizedNotExpired");
 			query.setParameter("token", userTokenString);
 			query.setParameter("deviceId", deviceIdString);
@@ -53,5 +66,25 @@ public final class HttpUtil {
 		}
 
 		return null;
+	}
+	
+	public String getBodyString(HttpServletRequest request) {
+		BufferedReader br;
+		try {
+			br = request.getReader();
+			String str, wholeStr = "";
+			while((str = br.readLine()) != null){
+				wholeStr += str;
+			}
+			return wholeStr;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void setStatus(HttpServletResponse response, Error error) {
+		
 	}
 }
